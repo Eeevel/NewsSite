952f298f6c121fcd6657d052e71ca42f304df209
Pavel <pavel.shalyga.1992@gmail.com>
2021-07-09 22:40:57 +0300

    Add article validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :short_description, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+  validates :category, presence: true
+  validates :region, presence: true
 end
