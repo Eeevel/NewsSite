966869e677c410f74daf671be80b84b8b1ee355e
Pavel <pavel.shalyga.1992@gmail.com>
2021-07-13 17:32:37 +0300

    Add category field for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,11 @@
 class Article < ApplicationRecord
   include Status
+  include Category
 
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :short_description, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-  validates :category, presence: true
   validates :region, presence: true
 end
