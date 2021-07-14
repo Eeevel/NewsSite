1e76ba69fb6853ad2eb3af191c88888b6b9fe72d
Pavel <pavel.shalyga.1992@gmail.com>
2021-07-14 17:40:36 +0300

    Add news_main_image, rating and redactor fields for Articles migration

--- db/migrate/20210709190944_create_articles.rb
+++ db/migrate/20210709190944_create_articles.rb
@@ -4,12 +4,14 @@ class CreateArticles < ActiveRecord::Migration[6.1]
       t.string :title
       t.text :short_description
       t.text :body
+      t.string :news_main_image
       t.string :category
       t.string :region
       t.string :status
       t.boolean :important
-      t.float :rating
+      t.integer :rating
       t.float :average_rating
+      t.string :redactor
 
       t.timestamps
     end
