d06d9c7eba78881dce6f89e6479d7b7d40c934d8
Pavel <pavel.shalyga.1992@gmail.com>
2021-07-14 18:05:04 +0300

    Add news_main_image validation and mount_uploader for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,5 +7,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :short_description, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+  validates :news_main_image, presence: true
   validates :region, presence: true
+
+  mount_uploader :news_main_image, NewsMainImageUploader
 end
