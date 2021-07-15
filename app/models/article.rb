ccb20fcf9e43f000d3ae4311d13ce5756f4dd869
Pavel <pavel.shalyga.1992@gmail.com>
2021-07-15 18:57:20 +0300

    Add rich text

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,9 +6,11 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :short_description, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :body, presence: true
   validates :news_main_image, presence: true
   validates :region, presence: true
 
   mount_uploader :news_main_image, NewsMainImageUploader
+
+  has_rich_text :body
 end
