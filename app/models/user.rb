cdeb76d362ca3ad16c88642a2862bbb2cd6f41d3
Pavel <pavel.shalyga.1992@gmail.com>
2021-07-28 13:52:55 +0300

    Add role field for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  enum role: %i[user admin redactor correspondent]
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable,
