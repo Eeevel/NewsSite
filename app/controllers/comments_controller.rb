5ea032bb1e5135f60e141c044e119b95794d83ce
Pavel <pavel.shalyga.1992@gmail.com>
2021-07-29 13:11:42 +0300

    Add authorize for Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,18 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.article = @article
+    authorize @comment
+    @comment.save
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
+    authorize @comment
     @comment.destroy
     redirect_to article_path(@article)
   end
@@ -15,6 +20,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:body)
   end
 end
