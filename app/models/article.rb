7be20cdc9b434c92db2a31cc7e65325e40b7b8d9
Pavel <pavel.shalyga.1992@gmail.com>
2021-07-31 13:16:08 +0300

    Add access_mask enum for Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,15 +5,17 @@ class Article < ApplicationRecord
   belongs_to :user
 
   has_many :comments, dependent: :destroy
-
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+
+  enum access_mask: %i[all_content title_and_short_description title nothing], _default: 'all_content'
 
   validates :title, presence: true
   validates :short_description, presence: true
   validates :body, presence: true
   validates :news_main_image, presence: true
   validates :region, presence: true
+  validates :access_mask, presence: true
 
   mount_uploader :news_main_image, NewsMainImageUploader
 
