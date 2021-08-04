5f9cd555277586b596c45897eaa10ff9dd706bf8
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-04 19:36:25 +0300

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   has_many :articles
   has_many :comments
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :nickname, presence: true
+  validates :address, presence: true
+  validates :date_of_birth, presence: true
+  validates :avatar, presence: true
+
+  mount_uploader :avatar, AvatarUploader
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable,
