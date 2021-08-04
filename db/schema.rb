18954841217753936a1c785e485ff665499cfb8b
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-04 19:34:51 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_104613) do
+ActiveRecord::Schema.define(version: 2021_08_04_143618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,10 @@ ActiveRecord::Schema.define(version: 2021_08_03_104613) do
     t.integer "role"
     t.string "first_name"
     t.string "last_name"
+    t.string "nickname"
+    t.string "address"
+    t.date "date_of_birth"
+    t.string "avatar"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
