ef2218e5cf47fa2445f313fe4ccf04890d33678b
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-05 13:01:07 +0300

    Add private field to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_143618) do
+ActiveRecord::Schema.define(version: 2021_08_05_095850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_143618) do
     t.string "address"
     t.date "date_of_birth"
     t.string "avatar"
+    t.boolean "private", default: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
