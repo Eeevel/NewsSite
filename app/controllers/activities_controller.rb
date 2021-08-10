96d451f41925f322d727a627ad1c01aa90ed2927
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-10 15:04:23 +0300

    Add pagy to Activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,6 @@ class ActivitiesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @comments = @user.comments.order('updated_at DESC')
-    @viewings = @user.viewings.order('updated_at DESC')
+    @pagy, @viewings = pagy(@user.viewings.order('updated_at DESC'), items: 10)
   end
 end
