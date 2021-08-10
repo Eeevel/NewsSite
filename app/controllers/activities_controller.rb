b511e386ad6cc76af5a852146a25f4560d239bb2
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-10 13:43:30 +0300

    Update Activities controller with Viewings

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,5 +2,6 @@ class ActivitiesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @comments = @user.comments.order('updated_at DESC')
+    @viewings = @user.viewings.order('updated_at DESC')
   end
 end
