bc26190915e732c98ba8e8d731842a87344d775f
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-10 16:21:22 +0300

    Add sorting to Activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,14 @@
 class ActivitiesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @comments = @user.comments.order('updated_at DESC')
+    @comments = case params[:sort]
+                when 'body_desc'
+                  @user.comments.order('body DESC')
+                when 'updated_at_desc'
+                  @user.comments.order('updated_at DESC')
+                else
+                  @user.comments.order(params[:sort])
+                end
     @pagy, @viewings = pagy(@user.viewings.order('updated_at DESC'), items: 10)
   end
 end
