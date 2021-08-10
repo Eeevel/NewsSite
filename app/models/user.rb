4069b299833869f4704a9572245cef98fd8fb394
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-10 12:30:17 +0300

    Add reference between User and Viewings

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :articles
   has_many :comments
   has_many :ratings
+  has_many :viewings
 
   validates :first_name, presence: true
   validates :last_name, presence: true
