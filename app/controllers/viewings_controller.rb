64a399133a4c1516c3181bb26e63802b3cb2abf8
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-10 13:18:26 +0300

    Update Viewings controller with create method

--- app/controllers/viewings_controller.rb
+++ app/controllers/viewings_controller.rb
@@ -1,2 +1,17 @@
 class ViewingsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @viewing = Viewing.new(viewing_params)
+    @viewing.user = current_user
+    @viewing.article = @article
+    authorize @viewing
+    @viewing.save
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def viewing_params
+    params.require(:viewing).permit(:view)
+  end
 end
