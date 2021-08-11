73740616755caa9889456d5b9e47bb2cca82fed6
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-11 13:07:43 +0300

    Add RSS route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   resources :tags, only: [:show]
   resources :categories, only: [:show]
+  resources :rss, only: [:index], format: 'rss'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
