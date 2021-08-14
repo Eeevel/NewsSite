4760a20e4a944dac5095fb92b075d124791fab1c
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-14 15:46:50 +0300

    Add Subscription mailer preview

--- test/mailers/previews/subscription_mailer_preview.rb
+++ test/mailers/previews/subscription_mailer_preview.rb
@@ -1,4 +1,11 @@
 # Preview all emails at http://localhost:3000/rails/mailers/subscription_mailer
 class SubscriptionMailerPreview < ActionMailer::Preview
-
+  def send_subscription_email
+    SubscriptionMailer
+      .with(subscription: Subscription.new(user: User.new(email: 'user@user', first_name: 'User', last_name: '123456'),
+                                           parameter: :category,
+                                           name: 'People',
+                                           frequency: :daily))
+      .send_subscription_email
+  end
 end
