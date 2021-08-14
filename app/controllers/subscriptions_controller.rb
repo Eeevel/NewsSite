517498654bdd71622344a7a6eee88512a9353311
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-14 12:45:39 +0300

    Update Subscription controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,41 @@
 class SubscriptionsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @subscriptions = @user.subscriptions.order('updated_at DESC')
+    authorize @subscriptions
+  end
+
+  def new
+    @subscription = Subscription.new
+    @user = User.find(params[:user_id])
+    @subscription.user = @user
+    authorize @subscription
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @subscription = Subscription.new(subscription_params)
+    @subscription.user = @user
+    authorize @subscription
+
+    if @subscription.save
+      redirect_to user_subscriptions_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @subscription = User.find(params[:user_id]).subscriptions.find(params[:id])
+    authorize @subscription
+    @subscription.destroy
+
+    redirect_to user_subscriptions_path
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:parameter, :name, :frequency)
+  end
 end
