f92aac5a846211f3eae87a5d1ef7cc423ee85cf9
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-14 12:46:14 +0300

    Add reference between Users and Subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :ratings
   has_many :viewings
+  has_many :subscriptions
 
   validates :first_name, presence: true
   validates :last_name, presence: true
