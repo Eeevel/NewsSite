fb8067e27787861598e0755ded56c8ec182d821c
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-15 13:48:47 +0300

    Add Reports to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :ratings
   has_many :viewings
   has_many :subscriptions
+  has_many :reports
 
   validates :first_name, presence: true
   validates :last_name, presence: true
