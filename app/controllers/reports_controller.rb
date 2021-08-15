2a3a6bf5f5df4d7dbda8c9db696cee2be80377b2
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-15 13:27:39 +0300

    Update Reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,24 @@
 class ReportsController < ApplicationController
+  def new
+    @report = Report.new
+    authorize @report
+  end
+
+  def create
+    @report = Report.new(report_params)
+    authorize @report
+
+    if @report.save
+      flash[:notice] = 'Report sent to email'
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:start_date, :end_date)
+  end
 end
