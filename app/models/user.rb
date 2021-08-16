9aac75a4ef94a1d5b666de90953b54bb1d1dfd59
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-16 18:02:31 +0300

    Add from_omniauth method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:github]
+
+  def self.from_omniauth(access_token)
+    user = User.where(email: access_token.info.email).first
+    user ||= User.create(email: access_token.info.email,
+                         password: Devise.friendly_token[0, 20])
+    user.first_name = access_token.info.name.split.first
+    user.last_name = access_token.info.name.split.last
+    user.nickname = access_token.info.nickname
+    user.address = access_token.info.location || 'Belarus'
+    user.date_of_birth = '1970-01-01'
+    user.avatar = user.avatar || File.open('app/assets/images/default_avatar.jpg')
+    user.uid = access_token.uid
+    user.provider = access_token.provider
+    user.skip_confirmation!
+    user.save
+    user
+  end
 end
