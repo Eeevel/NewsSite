6e95a58ee5424abb22baca57d8370ad7c4af4cb5
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-17 15:48:58 +0300

    Add scope locale to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: %i[index show edit update] do
-    resources :activities, only: [:index]
-    resources :subscriptions, only: %i[index new create destroy]
+
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+    root 'articles#index'
+
+    resources :users, only: %i[index show edit update] do
+      resources :activities, only: [:index]
+      resources :subscriptions, only: %i[index new create destroy]
+    end
+
+    resources :articles do
+      resources :comments
+      resources :ratings
+      resources :viewings, only: %i[create]
+      resources :charts, only: %i[index]
+    end
+
+    resources :tags, only: [:show]
+    resources :categories, only: [:show]
+    resources :rss, only: [:index], format: 'rss'
+    resources :rss_feeds, only: %i[index show new create destroy]
+    resources :reports, only: %i[new create]
   end
-
-  resources :articles do
-    resources :comments
-    resources :ratings
-    resources :viewings, only: %i[create]
-    resources :charts, only: %i[index]
-  end
-
-  resources :tags, only: [:show]
-  resources :categories, only: [:show]
-  resources :rss, only: [:index], format: 'rss'
-  resources :rss_feeds, only: %i[index show new create destroy]
-  resources :reports, only: %i[new create]
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
