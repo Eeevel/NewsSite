df683c74f5cda80e4321c244469856b7e935fec2
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-19 14:14:33 +0300

    Update Search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,12 @@
 class SearchController < ApplicationController
+  def index
+    @search_text = search_params[:query].strip
+    @articles = Article.order('updated_at DESC').where('title LIKE ?', "%#{@search_text}%")
+  end
+
+  private
+
+  def search_params
+    params.permit(:query)
+  end
 end
