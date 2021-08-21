27795192ac9cc178a297a9ec02e8cb780bdbc306
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-21 17:01:09 +0300

    Update Search controller with service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,7 @@
 class SearchController < ApplicationController
   def index
     @search_text = search_params[:query].strip
-
-    if Elasticsearch::Model.client.ping
-      @articles = Article.search_articles(@search_text)
-    else
-      @articles = Article.order('updated_at DESC')
-                         .where(status: :published)
-                         .where('title LIKE ? OR short_description LIKE ?', "%#{@search_text}%", "%#{@search_text}%")
-    end
+    @articles = SearchArticlesService.call(@search_text)
   end
 
   private
