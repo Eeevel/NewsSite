04712190ebed3ea089dcdf672f30ef92ae05d865
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-25 15:15:06 +0300

    Update articles_spec.rb with factory

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,53 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Articles', type: :request do
-  let(:user) do
-    User.create do |u|
-      u.email = 'test123@test123'
-      u.password = '123456'
-      u.first_name = 'Test'
-      u.last_name = 'Test'
-      u.nickname = 'Test'
-      u.address = 'Belarus'
-      u.date_of_birth = '1970-01-01'
-      u.avatar = File.open('app/assets/images/default_avatar.jpg')
-      u.role = 'correspondent'
-      u.uid = ''
-      u.provider = ''
-      u.skip_confirmation!
-    end
-  end
-
-  let(:redactor) do
-    User.create do |u|
-      u.email = 'test_redactor@test_redactor'
-      u.password = '123456'
-      u.first_name = 'Test'
-      u.last_name = 'Test'
-      u.nickname = 'Test'
-      u.address = 'Belarus'
-      u.date_of_birth = '1970-01-01'
-      u.avatar = File.open('app/assets/images/default_avatar.jpg')
-      u.role = 'redactor'
-      u.uid = ''
-      u.provider = ''
-      u.skip_confirmation!
-    end
-  end
-
-  let(:article) do
-    Article.create do |a|
-      a.title = 'test'
-      a.short_description = 'test'
-      a.body = 'test'
-      a.news_main_image = File.open('app/assets/images/default_avatar.jpg')
-      a.category = 'people'
-      a.region = 'Belarus'
-      a.status = 'published'
-      a.important = true
-      a.user = user
-    end
-  end
+  let(:user) { create(:user, role: 'correspondent', email: 'cor@cor') }
+  let(:redactor) { create(:user, role: 'redactor') }
+  let(:article) { create(:article, user: user) }
 
   let(:article_invalid_params) do
     {
