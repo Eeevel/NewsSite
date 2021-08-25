25f50322d337d2c413386b15fab62e0122a4cb7f
Pavel <pavel.shalyga.1992@gmail.com>
2021-08-25 15:14:35 +0300

    Update categories_controller_spec.rb with factory

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,36 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
-  let(:user) do
-    User.create do |u|
-      u.email = 'test123@test123'
-      u.password = '123456'
-      u.first_name = 'Test'
-      u.last_name = 'Test'
-      u.nickname = 'Test'
-      u.address = 'Belarus'
-      u.date_of_birth = '1970-01-01'
-      u.avatar = File.open('app/assets/images/default_avatar.jpg')
-      u.role = 'correspondent'
-      u.uid = ''
-      u.provider = ''
-      u.skip_confirmation!
-    end
-  end
-
-  let(:article) do
-    Article.create do |a|
-      a.title = 'test'
-      a.short_description = 'test'
-      a.body = 'test'
-      a.news_main_image = File.open('app/assets/images/default_avatar.jpg')
-      a.category = 'people'
-      a.region = 'Belarus'
-      a.status = 'published'
-      a.important = true
-      a.user = user
-    end
-  end
+  let(:article) { create(:article) }
 
   describe 'GET #show' do
     before do
